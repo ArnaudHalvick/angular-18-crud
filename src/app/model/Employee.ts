8a28a94361af6182951648140cee04920a84f2fc
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-09 14:43:00 -0300

    Create EmployeeModel TS

---
+++ src/app/model/Employee.ts
@@ -0,0 +1,19 @@
+export class EmployeeModel {
+  employeeId: number;
+  name: string;
+  city: string;
+  state: string;
+  emailId: string;
+  contactNo: string;
+  address: string;
+
+  constructor() {
+    this.employeeId = 0;
+    this.name = '';
+    this.city = '';
+    this.state = '';
+    this.emailId = '';
+    this.contactNo = '';
+    this.address = '';
+  }
+}
